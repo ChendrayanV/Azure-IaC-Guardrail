e0549ce0275aad8e152718bd7cf55fc8d45202a2
Chendrayan Venkatesan <venkatesan.chendrayan@gmail.com>
2026-06-12 09:15:17 +0100

    cosmetic change - removed identitfiers

--- test/fixtures/intellisense-ux-demo/dev.tfvars
+++ test/fixtures/intellisense-ux-demo/dev.tfvars
@@ -1,4 +1,4 @@
-subscription_id               = "00000000-0000-0000-0000-000000000000"
-tenant_id                     = "00000000-0000-0000-0000-000000000000"
+subscription_id               = "<SUBSCRIPTION-ID>"
+tenant_id                     = "<TENANT-ID>"
 public_network_access_enabled = true
 remote_debugging_enabled      = true
--- test/fixtures/intellisense-ux-demo/main.tf
+++ test/fixtures/intellisense-ux-demo/main.tf
@@ -2,11 +2,11 @@ resource "azurerm_linux_web_app" "demo" {
   name                = "app-guardrail-intellisense-demo"
   resource_group_name = "rg-guardrail-intellisense-demo"
   location            = "uksouth"
-  service_plan_id     = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-guardrail-intellisense-demo/providers/Microsoft.Web/serverfarms/plan-demo"
+  service_plan_id     = "/subscriptions/<SUBSCRIPTION-ID>/resourceGroups/rg-guardrail-intellisense-demo/providers/Microsoft.Web/serverfarms/plan-demo"
 
   https_only                                     = true
   public_network_access_enabled                  = var.public_network_access_enabled
-  virtual_network_subnet_id                      = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-network/providers/Microsoft.Network/virtualNetworks/vnet-demo/subnets/snet-app"
+  virtual_network_subnet_id                      = "/subscriptions/<SUBSCRIPTION-ID>/resourceGroups/rg-network/providers/Microsoft.Network/virtualNetworks/vnet-demo/subnets/snet-app"
   webdeploy_publish_basic_authentication_enabled = false
 
   identity {
